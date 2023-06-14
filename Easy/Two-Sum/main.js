ab0497f8d763831f313ac99919ee762ed1bdf71f
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-06-14 17:50:20 +0200

    Added best approach for this kind of task

--- Easy/Two-Sum/main.js
+++ Easy/Two-Sum/main.js
@@ -44,3 +44,17 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+//Best approach would be data structure Map
+var twoSum = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
